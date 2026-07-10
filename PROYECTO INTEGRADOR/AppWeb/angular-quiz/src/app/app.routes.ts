3e70772a51ad8764fd15ed78aebdece5111bf747
josthin45 <69522251+josthin45@users.noreply.github.com>
2026-07-10 17:54:17 -0500

    appweb--

--- PROYECTO INTEGRADOR/AppWeb/angular-quiz/src/app/app.routes.ts
+++
@@ -1,6 +0,0 @@
-import { Routes } from '@angular/router';
-import { QuizListComponent } from './components/quiz-list/quiz-list.component';
-
-export const routes: Routes = [
-  { path: '', component: QuizListComponent }
-];
--- PROYECTO INTEGRADOR/AppWeb/vue-cards/src/types/index.ts
+++
@@ -1,8 +0,0 @@
-export interface Tarjeta {
-  id: string;
-  usuario_id: string;
-  categoria_id: string;
-  pregunta: string;
-  respuesta: string;
-  nivel: number;
-}
